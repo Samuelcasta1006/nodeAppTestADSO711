99f4967a174d92c6e6257f935ddd6f0a6e61e15b
Proyectoformativo2873711 <alejoescobarsenaproyecto@gmail.com>
2025-05-21 23:06:57 -0500

    The entire guide 04 is finished

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(morgan('dev')); // la opción dev da la información principal. Combined
 // Routes
 //Ruta de usuarios para la V1 del API
 app.use('/api/v1/users', require('./api/v1/routes/user.routes'));//Ruta para users
+app.use('/api/v1/articles', require('./api/v1/routes/articles.routes'));//Ruta para articles
 
 // Se configura una ruta sencilla a través del método GET para probar
 // app.get('/', (req, res) => { // req=request: llegan datos al servidor
